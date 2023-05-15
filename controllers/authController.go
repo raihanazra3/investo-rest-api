8fd078994c16caed2ad9ebe4c67260c2842a5724
raihanazra3 <raihanazra3@gmail.com>
2023-05-16 04:03:43 +0700

    Login API

--- controllers/authController.go
+++ controllers/authController.go
@@ -5,6 +5,7 @@ import (
 	"investo-rest-api/models"
 
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Register(c *fiber.Ctx) error {
@@ -14,13 +15,43 @@ func Register(c *fiber.Ctx) error {
 		return err
 	}
 
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
 	user := models.User{
 		Nama:     data["nama"],
 		Email:    data["email"],
-		Password: data["password"],
+		Password: string(password),
 	}
 
 	database.DB.Create(&user)
+
+	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "Email belum daftar",
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Password Salah!",
+		})
+	}
 
 	return c.JSON(user)
 }
